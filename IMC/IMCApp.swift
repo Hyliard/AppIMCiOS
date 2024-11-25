35cc9e07688067a0f59d14f1d627642b3727cb0a
hyliard <luisgerardomartinezh@gmail.com>
2024-11-25 07:14:05 -0300

    Initial Commit

---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Hyliard on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
