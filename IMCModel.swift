9702622f9c4c3ce2ce72a8877ae513320204fd07
Luis Martinez <luisgerardomartinezh@gmail.com>
2025-05-19 17:11:16 -0300

    Update imc fitness

---
+++ IMCModel.swift
@@ -0,0 +1,8 @@
+//
+//  IMCModel.swift
+//  IMC
+//
+//  Created by Luis Martinez on 19/05/2025.
+//
+
+import Foundation
---
+++ IMCViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  IMCViewModel.swift
+//  IMC
+//
+//  Created by Luis Martinez on 19/05/2025.
+//
+
+import SwiftUI
+
+struct IMCViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCViewModel()
+}
